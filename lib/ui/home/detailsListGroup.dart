a2c2b2cf3e25bd6d9dd2abcd6792e4a5643d3254
Nicholas Mueller <34995238+nightMue@users.noreply.github.com>
2019-08-04 21:03:02 -0500

    Worked on detailsListGroup, see comment about possible issue

--- lib/ui/home/detailsListGroup.dart
+++ lib/ui/home/detailsListGroup.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:nike_app/data/tempData.dart';
 
 class DetailsListGroup extends StatefulWidget {
   @override
   _DetailsListGroupState createState() => _DetailsListGroupState();
 }
 
+/**
+ * Might need to put this into the homeBottom.dart so that the controllers for 
+ * each detail item will be affected from the box controller when scrolled
+ */
 class _DetailsListGroupState extends State<DetailsListGroup> {
+  PageController _ctrPageDetails;
+
+  @override
+  void initState() {
+    _ctrPageDetails = PageController(viewportFraction: 1.0);
+    //_ctrPageBox.addListener((){  
+    //  _ctrPageDetails.animateToPage(_ctrPageBox.page.round(), duration: Duration(milliseconds: 150), curve: Curves.linear);
+    //});
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //3 scrolling lists to give cool effect for details
+    return SizedBox(
+      height: 150,
+      child: PageView.builder(
+        physics: BouncingScrollPhysics(),
+        controller: _ctrPageDetails,//PageController(viewportFraction: 0.8),
+        itemCount: adidasDetails.length,
+        itemBuilder: (BuildContext context, int itemIndex) {
+          return _buildCarouselItemDetail(context, itemIndex);
+        },
+      ),
     );
+  }
+
+  Widget _buildCarouselItemDetail(BuildContext context, int itemIndex) {
+    return adidasDetails[itemIndex];
   }
 }
